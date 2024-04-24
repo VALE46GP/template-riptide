277ee02f5f2eacd763892d62e558160f690d10ea
Eric Callari <ericcallari1@gmail.com>
2024-04-24 14:01:39 -0700

    finishes embedded google map | switches app styles to dark mode

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
 function GoogleMap() {
-  return (
-    <div style={{ height: '400px', width: '100%' }}>
-      <iframe
-        width="100%"
-        height="100%"
-        frameBorder="0" style={{border:0}}
-        referrerPolicy="no-referrer-when-downgrade"
-        src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
-          &q=The+Riptide,3639+Taraval+St,San+Francisco,CA+94116`} allowFullScreen>
-      </iframe>
-    </div>
-  );
+    console.log('API KEY = ', process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
+    return (
+        <div style={{ height: '400px', width: '100%' }}>
+            <iframe
+                title="Google Map"
+                width="100%"
+                height="100%"
+                frameBorder="0" style={{border:0}}
+                referrerPolicy="no-referrer-when-downgrade"
+                src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+                    &q=The+Riptide,3639+Taraval+St,San+Francisco,CA+94116`} allowFullScreen>
+            </iframe>
+        </div>
+    );
 }
 
 export default GoogleMap;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,46 +7,43 @@ import GoogleMap from '../components/GoogleMap';
 
 function Home() {
     return (
-        <div>
+        <div className="home">
             <Hero />
-            <Container className="content" style={{ marginTop: '20px' }}>
-                <Row className="align-items-center">
+            <Container className="home__content" style={{ marginTop: '20px' }}>
+                <Row className="home__section">
                     <Col lg={12}>
                         <p><strong>The Riptide</strong> is the Bay Area's best little honky-tonk by the beach. Located in the Sunset District of San Francisco, it was built in 1941 and has the look and feel of a small town lodge. In addition to rustic brick flooring and high ceilings, the original knotty pine walls and raging fireplace add warmth you can feel as soon as you step inside.</p>
                         <p>The Riptide opens at 3 P.M. daily. If you’re looking for a casual atmosphere and a conversation or two with the local neighbors, stop by for Happy Hour, Monday through Friday, from 3 P.M. to 7 P.M. featuring honest drink specials.</p>
                         <p>Enjoy some of the best touring and local music acts including bluegrass, jazz, blues, solo acoustic, and country. Check the calendar for events or just wander in anytime.</p>
                         <p>The Riptide is only 20 minutes from the heart of San Francisco, one block away from the Pacific Ocean, half a block from public transportation with plenty of free parking.</p>
-                        <img className="text-image__image" src="https://template-riptide.s3.us-west-1.amazonaws.com/home-hero-OLD-v2.jpg" alt="Riptide interior" />
+                        <Image className="home__image" src="https://template-riptide.s3.us-west-1.amazonaws.com/home-hero-OLD-v2.jpg" alt="Riptide interior" />
                     </Col>
                 </Row>
-                <Row id="calendar" className="my-4">
-                <Col lg={12}>
-                    <h2>Calendar</h2>
-                    <div className="calendar-container">
-                        <Calendar />
-                    </div>
-                </Col>
-            </Row>
-            <Row id="directions" className="my-4">
-                <Col lg={12}>
-                    <GoogleMap />
-                </Col>
-            </Row>
-            <Row id="photos" className="my-4">
-                <Col lg={12}>
-                    <h2>Photos</h2>
-                    <Image src="https://template-riptide.s3.us-west-1.amazonaws.com/home-sunset-OLD-v2.jpg" alt="The Riptide" fluid />
-                    <p>Taraval Street was named after Sebastian Taraval, a Native American guide for Juan Bautista de Anza's 1775-1776 expedition to San Francisco.</p>
-                </Col>
-            </Row>
-            <Row className="my-4">
-                <Col lg={12}>
-                    <Image src="https://template-riptide.s3.us-west-1.amazonaws.com/home-carville-OLD-v2.jpg" alt="Sunset Carville" fluid />
-                    <h2>Sunset Carville</h2>
-                    <p>At the end of the 19th century, the city began replacing horse-drawn cars with electric streetcars. Dumped out near the beach in the Sunset, many cars were sold to individuals who paid $10 if the car had no seats and $20 if it did. People set up these cars along the sand at the Great Highway and turned them into homes. Some stacked two or three cars on top of one another for a multi-story home; others placed cars in a u-shape to create a courtyard protected from the wind. The area became known as "Carville-by-the-Sea" or simply "Carville." By 1901, 50 families lived in this unusual community that included a two-story church and a café. By the 1930s and 1940s, as development increased and property became more valuable, these cars disappeared. Today, two surviving houses are known to be built around streetcars. Others may exist as well.</p>
-                    <p>* The deepest part of the San Francisco Bay is right under the Golden Gate Bridge, with 330 feet of water, other parts of the bay can be as shallow as 7 feet.</p>
-                </Col>
-            </Row>
+                <Row className="home__section">
+                    <Col lg={12}>
+                        <h2>Calendar</h2>
+                        <div className="home__calendar-container">
+                            <Calendar />
+                        </div>
+                    </Col>
+                </Row>
+                <Row className="home__section">
+                    <Col lg={6}>
+                        <Image className="home__image" src="https://template-riptide.s3.us-west-1.amazonaws.com/home-carville-OLD-v3.jpg" alt="Sunset Carville" fluid />
+                        <h2>Sunset Carville</h2>
+                        <p>At the end of the 19th century, the city began replacing horse-drawn cars with electric streetcars. Dumped out near the beach in the Sunset, many cars were sold to individuals who paid $10 if the car had no seats and $20 if it did. People set up these cars along the sand at the Great Highway and turned them into homes. Some stacked two or three cars on top of one another for a multi-story home; others placed cars in a u-shape to create a courtyard protected from the wind. The area became known as "Carville-by-the-Sea" or simply "Carville." By 1901, 50 families lived in this unusual community that included a two-story church and a café. By the 1930s and 1940s, as development increased and property became more valuable, these cars disappeared. Today, two surviving houses are known to be built around streetcars. Others may exist as well.</p>
+                        <p>* The deepest part of the San Francisco Bay is right under the Golden Gate Bridge, with 330 feet of water, other parts of the bay can be as shallow as 7 feet.</p>
+                    </Col>
+                    <Col lg={6}>
+                        <Image className="home__image" src="https://template-riptide.s3.us-west-1.amazonaws.com/home-sunset-OLD-v2.jpg" alt="The Riptide" fluid />
+                        <p>Taraval Street was named after Sebastian Taraval, a Native American guide for Juan Bautista de Anza's 1775-1776 expedition to San Francisco.</p>
+                    </Col>
+                </Row>
+                <Row className="home__section" id="directions">
+                    <Col lg={12}>
+                        <GoogleMap />
+                    </Col>
+                </Row>
             </Container>
         </div>
     );
